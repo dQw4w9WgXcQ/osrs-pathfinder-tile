1ae23d97dbacb0791f30c1909353fb2162ded8b6
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2024-03-13 09:37:20 -0700

    tiebreak

--- src/lib.rs
+++ src/lib.rs
@@ -456,25 +456,27 @@ fn chebyshev(a: &Point, b: &Point) -> i32 {
     max(dx, dy)
 }
 
-fn heuristic(a: &Point, b: &Point) -> i32 {
-    chebyshev(a, b)
+// fn heuristic(a: &Point, b: &Point) -> i32 {
+//     chebyshev(a, b)
+// }
+
+//kinda works but not really.
+//a* w/ tiebreak: https://cdn.discordapp.com/attachments/167513997694861313/1217511021040762961/Screenshot_2024-03-13_at_9.33.45_AM.png
+//bfs: https://cdn.discordapp.com/attachments/167513997694861313/1217511070822961252/Screenshot_2024-03-13_at_9.33.36_AM.png
+
+fn manhattan(a: &Point, b: &Point) -> i32 {
+    let dx = (a.x - b.x).abs();
+    let dy = (a.y - b.y).abs();
+    dx + dy
 }
 
-//doesnt work
+//manhattan distance is used as a tiebreaker to create nicer paths
+fn heuristic(a: &Point, b: &Point) -> i32 {
+    let chebyshev = chebyshev(a, b);
+    let manhattan = manhattan(a, b);
 
-// fn manhattan(a: &Point, b: &Point) -> i32 {
-//     let dx = (a.x - b.x).abs();
-//     let dy = (a.y - b.y).abs();
-//     dx + dy
-// }
-//
-// //manhattan distance is used as a tiebreaker to create nicer paths
-// fn heuristic(a: &Point, b: &Point) -> i32 {
-//     let chebyshev = chebyshev(a, b);
-//     let manhattan = manhattan(a, b);
-//
-//     (chebyshev + 100_000) + manhattan
-// }
+    (chebyshev + 100_000) + manhattan
+}
 
 #[cfg(test)]
 mod tests {
